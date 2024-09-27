2c3b4860ac6f628de3150a035dc5294cf5961895
Melina Daversa <98925895+Melina334@users.noreply.github.com>
2024-09-27 20:44:49 -0300

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,43 +1,5 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application)
-}
-
-android {
-    namespace = "com.example.parcial_1_am_acn4v_ascencio_daversa"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.example.parcial_1_am_acn4v_ascencio_daversa"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-}
-
-dependencies {
-
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
 }
